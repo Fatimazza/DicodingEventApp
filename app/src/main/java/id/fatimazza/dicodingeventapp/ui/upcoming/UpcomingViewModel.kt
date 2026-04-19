0696becd73d543d459b7eed0e1b3e909ba1b094c
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 13:22:06 +0700

    Upcoming UI - Remove unused Text

--- app/src/main/java/id/fatimazza/dicodingeventapp/ui/upcoming/UpcomingViewModel.kt
+++ app/src/main/java/id/fatimazza/dicodingeventapp/ui/upcoming/UpcomingViewModel.kt
@@ -1,13 +1,8 @@
 package id.fatimazza.dicodingeventapp.ui.upcoming
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class UpcomingViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+
 }
