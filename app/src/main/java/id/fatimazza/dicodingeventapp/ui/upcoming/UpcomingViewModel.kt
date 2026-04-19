e807a7e5b6526a4702a51e3b3581008b67ff1d1f
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 21:02:02 +0700

    Upcoming VM - Get Upcoming Events in View Model

--- app/src/main/java/id/fatimazza/dicodingeventapp/ui/upcoming/UpcomingViewModel.kt
+++ app/src/main/java/id/fatimazza/dicodingeventapp/ui/upcoming/UpcomingViewModel.kt
@@ -3,13 +3,16 @@ package id.fatimazza.dicodingeventapp.ui.upcoming
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import id.fatimazza.dicodingeventapp.data.response.EventResponse
 import id.fatimazza.dicodingeventapp.data.response.ListEventsItem
+import id.fatimazza.dicodingeventapp.data.retrofit.ApiConfig
+import retrofit2.Callback
 
 class UpcomingViewModel : ViewModel() {
 
     companion object {
         private const val TAG = "UpcomingViewModel"
-        private const val IS_EVENT_ACTIVE = "1"
+        private const val IS_EVENT_ACTIVE = 1
     }
 
     private val _listUpcoming = MutableLiveData<List<ListEventsItem>>()
@@ -17,4 +20,15 @@ class UpcomingViewModel : ViewModel() {
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
+
+    init {
+        getUpcomingEvents()
+    }
+
+    private fun getUpcomingEvents() {
+        val client = ApiConfig.getApiService().getEvents(IS_EVENT_ACTIVE)
+        client.enqueue(object : Callback<EventResponse> {
+
+        })
+    }
 }
