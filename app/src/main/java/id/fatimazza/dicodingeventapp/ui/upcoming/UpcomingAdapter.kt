f1f3340d1727c59a891b91f7cb0dfbc20112e55f
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 14:26:49 +0700

    Upcoming Adapter - Make Adapter extends View Holder

--- app/src/main/java/id/fatimazza/dicodingeventapp/ui/upcoming/UpcomingAdapter.kt
+++ app/src/main/java/id/fatimazza/dicodingeventapp/ui/upcoming/UpcomingAdapter.kt
@@ -1,11 +1,26 @@
 package id.fatimazza.dicodingeventapp.ui.upcoming
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import id.fatimazza.dicodingeventapp.data.response.ListEventsItem
 import id.fatimazza.dicodingeventapp.databinding.ItemListEventsBinding
 
 class UpcomingAdapter: ListAdapter<ListEventsItem, UpcomingAdapter.ItemViewHolder> {
+    
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(
+        holder: ItemViewHolder,
+        position: Int
+    ) {
+        TODO("Not yet implemented")
+    }
 
     class ItemViewHolder(val binding: ItemListEventsBinding) : RecyclerView.ViewHolder(binding.root) {
 
