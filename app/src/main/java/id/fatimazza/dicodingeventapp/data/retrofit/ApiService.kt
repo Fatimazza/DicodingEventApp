a61e6df38aeef73e111917b006ab0ef5a826b328
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 20:58:45 +0700

    Network - Fix API Service to get Events request

--- app/src/main/java/id/fatimazza/dicodingeventapp/data/retrofit/ApiService.kt
+++ app/src/main/java/id/fatimazza/dicodingeventapp/data/retrofit/ApiService.kt
@@ -6,11 +6,9 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-class ApiService {
-    interface ApiService {
-        @GET("events")
-        fun getEvents(
-            @Query("active") active: Int
-        ): Call<EventResponse>
-    }
+interface ApiService {
+    @GET("events")
+    fun getEvents(
+        @Query("active") active: Int
+    ): Call<EventResponse>
 }
