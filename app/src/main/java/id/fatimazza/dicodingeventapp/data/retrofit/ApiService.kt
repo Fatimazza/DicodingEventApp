0961914d8f7a37e0ef65345761e0eb85dace6cae
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 17:31:18 +0700

    Network - Add API Service to get Events request

---
+++ app/src/main/java/id/fatimazza/dicodingeventapp/data/retrofit/ApiService.kt
@@ -0,0 +1,16 @@
+package id.fatimazza.dicodingeventapp.data.retrofit
+
+
+import id.fatimazza.dicodingeventapp.data.response.EventResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+class ApiService {
+    interface ApiService {
+        @GET("events")
+        fun getEvents(
+            @Query("active") active: Int
+        ): Call<EventResponse>
+    }
+}
