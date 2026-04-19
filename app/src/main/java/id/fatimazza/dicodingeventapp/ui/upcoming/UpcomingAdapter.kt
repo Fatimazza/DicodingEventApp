32e4ff535e1ce8ef945d5c5b2f33152c45e32395
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 14:29:39 +0700

    Upcoming Adapter - Add Diff Util Item Callback

--- app/src/main/java/id/fatimazza/dicodingeventapp/ui/upcoming/UpcomingAdapter.kt
+++ app/src/main/java/id/fatimazza/dicodingeventapp/ui/upcoming/UpcomingAdapter.kt
@@ -1,13 +1,15 @@
 package id.fatimazza.dicodingeventapp.ui.upcoming
 
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import id.fatimazza.dicodingeventapp.data.response.ListEventsItem
 import id.fatimazza.dicodingeventapp.databinding.ItemListEventsBinding
 
-class UpcomingAdapter: ListAdapter<ListEventsItem, UpcomingAdapter.ItemViewHolder> {
-    
+class UpcomingAdapter:
+    ListAdapter<ListEventsItem, UpcomingAdapter.ItemViewHolder>(DIFF_CALLBACK) {
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -24,5 +26,23 @@ class UpcomingAdapter: ListAdapter<ListEventsItem, UpcomingAdapter.ItemViewHolde
 
     class ItemViewHolder(val binding: ItemListEventsBinding) : RecyclerView.ViewHolder(binding.root) {
 
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ListEventsItem>(){
+            override fun areItemsTheSame(
+                oldItem: ListEventsItem,
+                newItem: ListEventsItem
+            ): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun areContentsTheSame(
+                oldItem: ListEventsItem,
+                newItem: ListEventsItem
+            ): Boolean {
+                TODO("Not yet implemented")
+            }
+        }
     }
 }
