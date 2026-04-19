b05b067f0774307e66ad22a167cd53ae3c42c4da
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-04-19 17:46:59 +0700

    Upcoming VM - Add needed variables in View Model

--- app/src/main/java/id/fatimazza/dicodingeventapp/ui/upcoming/UpcomingViewModel.kt
+++ app/src/main/java/id/fatimazza/dicodingeventapp/ui/upcoming/UpcomingViewModel.kt
@@ -1,8 +1,20 @@
 package id.fatimazza.dicodingeventapp.ui.upcoming
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import id.fatimazza.dicodingeventapp.data.response.ListEventsItem
 
 class UpcomingViewModel : ViewModel() {
 
+    companion object {
+        private const val TAG = "UpcomingViewModel"
+        private const val IS_EVENT_ACTIVE = "1"
+    }
 
+    private val _listUpcoming = MutableLiveData<List<ListEventsItem>>()
+    val listUpcoming: LiveData<List<ListEventsItem>> = _listUpcoming
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
 }
